68141bdee17c48c39f9cee3ed0f002ed860923d0
Emanuel <emanuel.pais@econo.unlp.edu.ar>
2019-06-25 19:14:59 -0300

    Agregue el manejo del parametro de la tutoria

--- dev/src/tutorias-ui/src/app/modules/tutorias/asistencia/nueva/seleccionar-persona/seleccionar-persona.component.ts
+++ dev/src/tutorias-ui/src/app/modules/tutorias/asistencia/nueva/seleccionar-persona/seleccionar-persona.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { NavegarService } from '../../../../../core/navegar.service';
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-seleccionar-persona',
@@ -8,23 +11,44 @@ import { NavegarService } from '../../../../../core/navegar.service';
 })
 export class SeleccionarPersonaComponent implements OnInit {
 
-  constructor(private navegar: NavegarService) { }
+  subscriptions = [];
+  id$: Observable<string>;
+
+  constructor(
+    private navegar: NavegarService,
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit() {
+    this.id$ = this.route.paramMap.pipe(
+      switchMap( params => {
+        if (params.has('id')) {
+          return params.get('id');
+        } else {
+          return null;
+        }
+      })
+    )
   }
 
+  ngOnDestroy(): void {
+    this.subscriptions.forEach(s => s.unsubscribe());
+  }  
+
   volver() {
-    this.navegar.volver().subscribe().unsubscribe();
+    this.subscriptions.push(this.navegar.volver().subscribe());
   }
 
   seleccionado(personas) {
     let ids = personas.map( p => p.id);
-    let s = this.navegar.navegar({
-      url: '/sistema/tutorias/asistencia/nueva/alta/2',
-      params: { ids: ids}
-    }).subscribe(_ => {
-      s.unsubscribe();
-    })
+    this.subscriptions.push(this.id$.pipe(
+      switchMap( id => {
+        return this.navegar.navegar({
+          url: '/sistema/tutorias/asistencia/nueva/alta/'+id,
+          params: { ids: ids}
+        })
+      })
+    ).subscribe());
   }
 
 }
